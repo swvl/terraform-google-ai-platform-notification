369d22a0ed7d2520998ebcf4c4ecb9fc045b8107
Ahmed Abdelwahab <ahmed.abdelwahab@swvl.com>
2021-05-23 20:16:57 +0200

    feat: Send slack message

--- functions/main.py
+++ functions/main.py
@@ -1,28 +1,36 @@
 import base64
 import json
 import requests
+import re
 import os
-from typing import Dict, Optional
-from data import Event, Data, JobState, JsonPayload
-from _logging import get_logger
+from typing import Dict
+from .data import Event, Data, JobState, JsonPayload
+from ._logging import get_logger
+from googleapiclient.discovery import build
+
+ml = build('ml', 'v1')
+GCP_PROJECT = os.environ.get('GCP_PROJECT')
 
 
-def check_job_state(data: Data) -> Optional[JobState]:
+def check_job_state(data):
+    logger = get_logger()
+    projectName = data.resource.labels.project_id
+    projectId = 'projects/{}'.format(projectName)
+    jobName = data.resource.labels.job_id
+    jobId = '{}/jobs/{}'.format(projectId, jobName)
+    request = ml.projects().jobs().get(name=jobId)
+    response = None
+    try:
+        response = request.execute()
+    except Exception as err:
+        # Something went wrong. Handle the exception in an appropriate
+        #  way for your application.
+        raise err
+    if response is None:
+        raise Exception(f'Got no response for {jobId}')
+    logger.info(f'Response: {json.dumps(response)}')
+    return response
 
-    if isinstance(data.textPayload, str):
-        if "completed successfully" in data.textPayload:
-            return JobState.SUCCEEDED
-        elif "failed" in data.textPayload:
-            return JobState.FAILED
-        elif "cancelled" in data.textPayload:
-            return JobState.CANCELLED
-
-    elif isinstance(data.jsonPayload, JsonPayload):
-        if "queued" in data.jsonPayload.message:
-            return JobState.QUEUED
-
-    else:
-        return None
 
 def get_slack_user_name(email: str) -> str:
     bot_token = os.environ.get('SLACK_BOT_TOKEN')
@@ -36,19 +44,29 @@ def get_slack_user_name(email: str) -> str:
         )
     return slack_user_name
 
+def get_message(message: Dict) -> str:
+    global GCP_PROJECT
+    slack_message_template = "{slack_user_name} => JOB *{job_id}* state is: `{job_state}`{details}\n{job_link} "
+    email = message.get('email')
+    slack_user_name = ''
+    if email:
+        slack_user_name = f'@{get_slack_user_name(email)}' 
+    details = message.get('job_error_message', '')
+    if details:
+        details = f'\n> `{details}`\n'
+    job_link = f'https://console.cloud.google.com/ai-platform/jobs/{message["job_id"]}/charts/cpu?project={GCP_PROJECT}'
+    return slack_message_template.format(slack_user_name=slack_user_name, job_id=message['job_id'], job_state=message['job_state'], details=details, job_link=job_link) 
+
 def send_message(message: Dict) -> None:
     # Set the webhook_url to the one provided by Slack when you create the webhook at https://my.slack.com/services/new/incoming-webhook/
     webhook_url = os.environ.get('SLACK_WEBHOOK_URI')
     channel_name = os.environ.get('SLACK_CHANNEL_NAME')
-    slack_message_template = "@{slack_user_name} => JOB *{job_id}* state is: `{job_state}`"
-    email = 'ahmed.abdelwahab@swvl.com'
-    slack_user_name = get_slack_user_name(email)
     response = requests.post(
         webhook_url, data=json.dumps({
             'parse': 'full',
             'link_names': 1,
             'channel': channel_name,
-            'text': slack_message_template.format(slack_user_name=slack_user_name, job_id=message['job_id'], job_state=message['job_state'])     
+            'text': get_message(message)     
         }),
         headers={'Content-Type': 'application/json'}
     )
@@ -58,49 +76,31 @@ def send_message(message: Dict) -> None:
             % (response.status_code, response.text)
         )
 
-def maybe_publish_message(message: Dict) -> None:
+def extract_email_from_job(job):
+    args = job['trainingInput']['args']
+    for arg in args:
+        result = re.search('--email(?:=| )(.+)', arg)
+        if result:
+            return result.group(1)
+    return None
 
+def main(event, context):
+    """Triggered from a message on a Cloud Pub/Sub topic.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
     logger = get_logger()
-
+    pubsub_message = base64.b64decode(event['data']).decode('utf-8')
+    event_data = json.loads(pubsub_message)
+    data = Data(**event_data)
+    logger.info(f'data: {json.dumps(event_data)}')
+    job = check_job_state(data)
+    email = extract_email_from_job(job)
+    message = {'email': email, 'job_id': job['jobId'], 'job_error_message': job.get('errorMessage'), 'job_state': job['state']}
     send_message(message)
-    logger.info("Message was published.")
 
 
-def main(event_dict: Dict, context) -> Dict:
 
-    logger = get_logger()
 
-    logger.info(f"Event: {json.dumps(event_dict, indent=2)}")
-    logger.info(f"Context: {context}")
-
-    # Cast event from dict to Event instance.
-    event = Event(**event_dict)
-
-    # Extract Base64 data from event.
-    data_str = base64.b64decode(event.data).decode("utf-8").strip()
-    data_dict = json.loads(data_str)
-
-    logger.info(f"Data: {json.dumps(data_dict, indent=2)}")
-
-    # Cast data from dict to Data instance.
-    data = Data(**data_dict)
-
-    job_state = check_job_state(data)
-
-    if job_state is None:
-        logger.info(f"Message was not published because job state is {job_state}")
-        return {}
-
-    output_message = {
-        "job_id": data.resource.labels.job_id,
-        "project_id": data.resource.labels.project_id,
-        "timestamp": data.timestamp,
-        "job_state": job_state.value,
-    }
-
-    logger.info(f"Output message: {json.dumps(output_message, indent=2)}")
-
-    # Publish message to Pub/Sub topic for notification.
-    maybe_publish_message(output_message)
-
-    return {}
+main({ "@type": "type.googleapis.com/google.pubsub.v1.PubsubMessage", "attributes": { "logging.googleapis.com/timestamp": "2021-05-23T10:45:21.131Z" }, "data": "eyJpbnNlcnRJZCI6Ii1zMG54eWRjeG1hIiwianNvblBheWxvYWQiOnsibWVzc2FnZSI6IlZhbGlkYXRpbmcgam9iIHJlcXVpcmVtZW50cy4uLiJ9LCJsYWJlbHMiOnsibWwuZ29vZ2xlYXBpcy5jb20vZW5kcG9pbnQiOiJtbC5nb29nbGVhcGlzLmNvbSJ9LCJsb2dOYW1lIjoicHJvamVjdHMvc3d2bC1zYW5kYm94L2xvZ3MvbWwuZ29vZ2xlYXBpcy5jb20lMkZ0ZXN0X2JpZ3NlYXJjaF8xIiwicmVjZWl2ZVRpbWVzdGFtcCI6IjIwMjEtMDUtMjNUMTA6NDU6MjEuNjM2MDA2ODY2WiIsInJlc291cmNlIjp7ImxhYmVscyI6eyJqb2JfaWQiOiJ0ZXN0X2JpZ3NlYXJjaF8xIiwicHJvamVjdF9pZCI6InN3dmwtc2FuZGJveCIsInRhc2tfbmFtZSI6InNlcnZpY2UifSwidHlwZSI6Im1sX2pvYiJ9LCJzZXZlcml0eSI6IklORk8iLCJ0aW1lc3RhbXAiOiIyMDIxLTA1LTIzVDEwOjQ1OjIxLjEzMVoifQ==" }, None)
