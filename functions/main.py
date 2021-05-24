896e8572927972a5c1184b31bd9f18c95e1e6fd3
Ahmed Abdelwahab <ahmed.abdelwahab@swvl.com>
2021-05-24 11:54:07 +0200

    fix: Use more robusta status checker

--- functions/main.py
+++ functions/main.py
@@ -3,16 +3,36 @@ import json
 import requests
 import re
 import os
-from typing import Dict
-from data import Event, Data, JobState, JsonPayload
-from _logging import get_logger
+from typing import Dict, Optional
+from .data import Event, Data, JobState, JsonPayload
+from ._logging import get_logger
 from googleapiclient.discovery import build
 
 ml = build('ml', 'v1')
 GCP_PROJECT = os.environ.get('GCP_PROJECT')
 
 
-def check_job_state(data):
+
+
+def check_job_state(data: Data) -> Optional[JobState]:
+
+    if isinstance(data.textPayload, str):
+        if "completed successfully" in data.textPayload:
+            return JobState.SUCCEEDED
+        elif "failed" in data.textPayload:
+            return JobState.FAILED
+        elif "cancelled" in data.textPayload:
+            return JobState.CANCELLED
+
+    elif isinstance(data.jsonPayload, JsonPayload):
+        if "queued" in data.jsonPayload.message:
+            return JobState.QUEUED
+
+    else:
+        return None
+
+
+def get_job_json(data):
     logger = get_logger()
     projectName = data.resource.labels.project_id
     projectId = 'projects/{}'.format(projectName)
@@ -52,9 +72,11 @@ def get_message(message: Dict) -> str:
     slack_user_name = ''
     if email:
         slack_user_name = f'@{get_slack_user_name(email)}' 
-    details = message.get('job_error_message', '')
+    details = message.get('job_error_message')
     if details:
         details = f'\n> `{details}`\n'
+    else:
+        details = ''
     job_link = f'https://console.cloud.google.com/ai-platform/jobs/{message["job_id"]}/charts/cpu?project={GCP_PROJECT}'
     return slack_message_template.format(slack_user_name=slack_user_name, job_id=message['job_id'], job_state=message['job_state'], details=details, job_link=job_link) 
 
@@ -97,7 +119,13 @@ def main(event, context):
     event_data = json.loads(pubsub_message)
     data = Data(**event_data)
     logger.info(f'data: {json.dumps(event_data)}')
-    job = check_job_state(data)
+    job = get_job_json(data)
+    job_state = check_job_state(data)
+    if not job_state:
+        logger.info('Job state is none, not sending a message')
+        return
     email = extract_email_from_job(job)
-    message = {'email': email, 'job_id': job['jobId'], 'job_error_message': job.get('errorMessage'), 'job_state': job['state']}
+    message = {'email': email, 'job_id': job['jobId'], 'job_error_message': job.get('errorMessage'), 'job_state': job_state}
     send_message(message)
+
+main({"@type": "type.googleapis.com/google.pubsub.v1.PubsubMessage", "attributes": {"logging.googleapis.com/timestamp": "2021-05-23T23:57:10.343201317Z"}, "data": "eyJpbnNlcnRJZCI6IjEweDJhcmtjdGM1IiwibGFiZWxzIjp7Im1sLmdvb2dsZWFwaXMuY29tL2VuZHBvaW50IjoiIn0sImxvZ05hbWUiOiJwcm9qZWN0cy9zd3ZsLXNhbmRib3gvbG9ncy9tbC5nb29nbGVhcGlzLmNvbSUyRnRlc3RfZHVyYXRpb25zX3BhcmFsbGVsXzEiLCJyZWNlaXZlVGltZXN0YW1wIjoiMjAyMS0wNS0yM1QyMzo1NzoxMS4xMzE3OTIyMjFaIiwicmVzb3VyY2UiOnsibGFiZWxzIjp7ImpvYl9pZCI6InRlc3RfZHVyYXRpb25zX3BhcmFsbGVsXzEiLCJwcm9qZWN0X2lkIjoic3d2bC1zYW5kYm94IiwidGFza19uYW1lIjoic2VydmljZSJ9LCJ0eXBlIjoibWxfam9iIn0sInNldmVyaXR5IjoiSU5GTyIsInRleHRQYXlsb2FkIjoiV2FpdGluZyBmb3IgdHJhaW5pbmcgcHJvZ3JhbSB0byBzdGFydC4iLCJ0aW1lc3RhbXAiOiIyMDIxLTA1LTIzVDIzOjU3OjEwLjM0MzIwMTMxN1oifQ=="}, None)
